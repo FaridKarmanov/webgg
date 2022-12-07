38687af8c034a2642e08e8fe290549189af0efc0
Farid <faridkarmanov@gmail.com>
2022-12-07 13:29:46 +0300

    add text

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,24 @@
+const dropdown = document.querySelector (".footer__container")
 
+
+
+if(dropdown) {
+        dropdown.addEventListener("click", (event) => {
+          if(event.target.tagName === "IMG" )   {
+            const id = event.target.id
+            const description = document.getElementById(`${id + '' + id}`)
+
+            if(description) {
+if(getComputedStyle(description).display === 'none') {
+    description.style.display = "block"
+} else     description.style.display = "none"
+
+                
+                
+            }
+
+          } 
+
+    }) 
+}
 
